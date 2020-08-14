4bf547963407833e1931bf871b0e5543ef714ae0
eran.saranga@gmail.com <eran.saranga@gmail.com>
2020-08-14 17:02:08 +0300

    handled unknown case

--- scrapper/ScrapperFactory.py
+++ scrapper/ScrapperFactory.py
@@ -1,6 +1,15 @@
 from scrapper.QuizmeScrapper import  QuizmeScrapper
 from urllib.parse import urlparse
 
+
+class UnknownQuizException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
+    pass
+
 def get_domain_from_url(url):
     parsed_uri = urlparse(url)
     result = '{uri.netloc}'.format(uri=parsed_uri)
@@ -9,5 +18,8 @@ def get_domain_from_url(url):
 
 def ScrapperChooser(url):
     scrappers = {'www.quizme.co.il': QuizmeScrapper}
-    return scrappers[get_domain_from_url(url)](url)
+    try:
+        return scrappers[get_domain_from_url(url)](url)
+    except:
+        raise UnknownQuizException('could not find a scrapper for the url quiz')
 
